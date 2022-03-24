d6a580ea1aa6555e6805dcac35ed8f11af8d545c
nILruM <inilrum@gmail.com>
2022-03-24 20:17:55 +0500

    Go to FetchContent

--- Fetch.cmake
+++ Fetch.cmake
@@ -21,7 +21,7 @@ if(NOT DEFINED BUILD_TEST)
     set(BUILD_TEST ON)
 endif()
 
-#include(FetchContent)
+include(FetchContent)
 
 #Parameters
 macro(Declare NAME REP INCL ADD)
@@ -36,16 +36,16 @@ macro(Declare NAME REP INCL ADD)
     message("Define ${NAME}_INCL variable with value: ${${NAME}_INCL} ")
 
     #Вариант с иcпользованием FetchContent
-    #FetchContent_Declare(${NAME}
-    #        GIT_REPOSITORY https://${REP}.git
-    #        GIT_TAG origin/master
-    #        SOURCE_DIR ${${NAME}_DIR})
-    #FetchContent_Populate(${NAME})
+    FetchContent_Declare(${NAME}
+            GIT_REPOSITORY https://${REP}.git
+            GIT_TAG origin/master
+            SOURCE_DIR ${${NAME}_DIR})
+    FetchContent_Populate(${NAME})
 
     #Вариант с использованием просто клонирования гит
-    if(NOT EXISTS ${${NAME}_DIR})
-        exec_program("git" "" ARGS "clone --depth=1 https://${REP}.git ${${NAME}_DIR}")
-    endif()
+    #if(NOT EXISTS ${${NAME}_DIR})
+    #    exec_program("git" "" ARGS "clone --depth=1 https://${REP}.git ${${NAME}_DIR}")
+    #endif()
     if(${ADD})
         add_subdirectory(${${NAME}_DIR} ${CMAKE_BINARY_DIR}/${PROJECT_NAME}/Libs/${NAME})
     endif()
